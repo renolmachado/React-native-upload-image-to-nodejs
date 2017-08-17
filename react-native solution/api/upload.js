883ead3811e346e5eefc88ffe9d2ebea1e60fdd2
Renol <renol@Renols-Mac.local>
2017-08-17 19:02:33 -0300

    Configurar IOs

--- react-native solution/api/upload.js
+++ react-native solution/api/upload.js
@@ -1,7 +1,7 @@
 import RNFetchBlob from 'react-native-fetch-blob'
 
 let upload = (data) => {
-  return RNFetchBlob.fetch('POST', 'http://192.168.1.47:3000/upload', {
+  return RNFetchBlob.fetch('POST', 'http://192.168.131.128:3000/upload', {
     Authorization : "Bearer access-token",
     otherHeader : "foo",
     'Content-Type' : 'multipart/form-data',
