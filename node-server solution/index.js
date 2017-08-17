e05424289e1ad20eb58fc442f35c2cc50b55af14
Renol Machado <renol94@gmail.com>
2017-08-17 11:11:44 -0300

    Crear endpoint para revolver imagen de la aplicación

--- node-server solution/index.js
+++ node-server solution/index.js
@@ -4,15 +4,23 @@ var fs = require('fs');
 var app = express();
 
 app.use(formidable({
-	uploadDir: 'public'
+    uploadDir: 'public'
 }));
 
 app.listen(3000, () => console.log('Server running'));
 
 app.post('/upload', (req, res) => {
-	fs.rename(req.files.avatar.path, req.files.avatar.path + '.jpg',
-	err => {
-		res.send('');
-	});
-	
+    fs.rename(req.files.avatar.path, req.files.avatar.path + '.jpg',
+        err => {
+            res.send('');
+        });
+
 })
+
+app.get('/images/:image_name', function (req, res) {
+    fs.readFile('node-server solution/public/' + req.params.image_name, function (err, data) {
+        if (err) throw err; // Fail if the file can't be read.
+        res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+        res.end(data); // Send the file data to the browser.
+    });
+});
